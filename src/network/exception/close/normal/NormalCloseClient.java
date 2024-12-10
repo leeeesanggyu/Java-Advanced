2de13fe8f518720bc21e8c0ba380d7a544d38b7a
이상규 <76906458+leeeesanggyu@users.noreply.github.com>
2024-12-10 21:54:09 +0900

    TCP 통신 종료 설명 추가

--- src/network/exception/close/normal/NormalCloseClient.java
+++ src/network/exception/close/normal/NormalCloseClient.java
@@ -5,6 +5,14 @@ import java.net.Socket;
 
 import static util.MyLogger.log;
 
+/**
+ * TCP 연결을 종료하려면 서로 FIN 메세지를 보내야 한다.
+ * 1. A가 B로 FIN 메세지를 보낸다.
+ * 2. FIN 메세지를 받은 B도 FIN 메세지를 보낸다.
+ *
+ * socket.close()를 호출하면 TCP에서 종료의 의미인 FIN 패킷을 상대방에게 전달한다.
+ * FIN 패킷을 받으면 상대방도 socket.close()를 호출해서 FIN 패킷을 상대방에게 전달해야 한다.
+ */
 public class NormalCloseClient {
 
     public static void main(String[] args) throws IOException {
